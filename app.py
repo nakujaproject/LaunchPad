2368888bca648637107fed805960f7f89d49fda1
sammy <sammyoina@gmail.com>
2021-04-22 23:43:07 +0300

    simple setup for initial tests

---
+++ app.py
@@ -0,0 +1,68 @@
+'''
+	Raspberry Pi GPIO Status and Control
+'''
+import RPi.GPIO as GPIO
+from flask import Flask, render_template, request
+from modules.ignition import *
+import time
+
+app = Flask(__name__)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+#define actuators GPIOs
+ledRed = 19
+
+#initialize GPIO status variables
+ledRedSts = 0
+
+# Define led pins as output
+GPIO.setup(ledRed, GPIO.OUT)   
+
+# turn leds OFF 
+GPIO.output(ledRed, GPIO.LOW)
+
+testNum = 0
+
+ignitionThreads = []
+
+@app.route("/")
+def index():
+	global testNum
+	# Read GPIO Status
+	ledRedSts = GPIO.input(ledRed)
+
+	templateData = {
+      'ledRed'  : ledRedSts,
+	  'testNum' : testNum,
+      }
+	return render_template('index.html', **templateData)
+	
+# The function below is executed when someone requests a URL with the actuator name and action in it:
+@app.route("/<deviceName>/<action>")
+def action(deviceName, action):
+	global testNum
+	if deviceName == 'ledRed':
+		actuator = ledRed
+   
+	if action == "on":
+		t1 = Ignition(actuator)
+		ignitionThreads.append(t1)
+		ignitionThreads[testNum].start()
+		ignitionThreads[testNum].stop()
+		testNum = testNum + 1
+	
+	if action == "off":
+		GPIO.output(actuator, GPIO.LOW)
+		     
+	ledRedSts = GPIO.input(ledRed)
+   
+	templateData = {
+      'ledRed'  : ledRedSts,
+	  'testNum' : testNum,
+	}
+	return render_template('index.html', **templateData)
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=80, debug=True)
