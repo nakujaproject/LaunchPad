c67ebae8394b879903e408b27b72187b95bed80d
sammy <sammyoina@gmail.com>
2021-04-28 23:07:20 +0300

    make list checkable

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, request
-import time
 
 app = Flask(__name__)
 
+#propulsion task bar
 propulsionProgress = 0
+
+#propulsion number of tasks
+propulsionNumTasks = 6
+
+#propulsion task list
+propulsionTaskList = [""] * propulsionNumTasks
 
 @app.route("/")
 def index():
@@ -14,11 +20,38 @@ def index():
 	return render_template('index.html', **templateData)
 	
 
-@app.route("/propulsion")
-def propulsionIndex():
+@app.route("/<group>")
+def groupIndex(group):
+    if group == "propulsion" :
+        templateData = {
+            'propulsionPercentage' : propulsionProgress,
+            'propulsionTask1' : propulsionTaskList[0],
+            'propulsionTask2' : propulsionTaskList[1],
+            'propulsionTask3' : propulsionTaskList[2],
+            'propulsionTask4' : propulsionTaskList[3],
+            'propulsionTask5' : propulsionTaskList[4],
+            'propulsionTask6' : propulsionTaskList[5],
+        }
+        return render_template('propulsion.html', **templateData)
+
+@app.route("/<group>/<task>/<taskNum>")
+def propulsionTasks(group, task, taskNum):
+    # complete a task and check it
+    taskNum = int(taskNum)
+    if propulsionTaskList[taskNum - 1] == "":
+        propulsionTaskList[taskNum - 1] = "checked"
+    # remove task if unchecked
+    else:
+        propulsionTaskList[taskNum - 1] = 0
     templateData = {
-        'propulsionPercentage' : propulsionProgress,
-    }
+            'propulsionPercentage' : propulsionProgress,
+            'propulsionTask1' : propulsionTaskList[0],
+            'propulsionTask2' : propulsionTaskList[1],
+            'propulsionTask3' : propulsionTaskList[2],
+            'propulsionTask4' : propulsionTaskList[3],
+            'propulsionTask5' : propulsionTaskList[4],
+            'propulsionTask6' : propulsionTaskList[5],
+        }
     return render_template('propulsion.html', **templateData)
 
 if __name__ == "__main__":
